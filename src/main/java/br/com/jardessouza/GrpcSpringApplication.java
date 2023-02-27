5c878e9d970d9419b41e9fcc2d24334dc224d066
Jardes Souza <jardessouza@gmail.com>
2023-02-27 16:54:54 -0300

    Initial settings

---
+++ src/main/java/br/com/jardessouza/GrpcSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.jardessouza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jardessouza/GrpcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jardessouza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
